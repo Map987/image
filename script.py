8ab137dca4609277c1c258d2d55b2d7ebb58a941
Map987 <99967856+Map987@users.noreply.github.com>
2024-04-20 03:10:18 +0800

    Update script.py

--- script.py
+++ script.py
@@ -2,38 +2,49 @@ import os
 import re
 import requests
 from bs4 import BeautifulSoup
+from datetime import datetime
 
 # sitemap URL
 sitemap_url = 'http://cineport.jp/post-sitemap.xml'
 
-# 下载图片的函数，同时处理URL
-def download_image(image_url, save_folder, txt_file):
-    # 删除URL中的"-scaled"和分辨率部分（如"-724x1024”），但只有当分辨率后缀后面没有其他字符时
-    pattern = r'(-\d+x\d+)(?![-])'
+# 处理下载原图URL的函数
+def process_main_image_url(image_url):
+    pattern = r'(-\d+x\d+)(?![\w-])'
     processed_url = re.sub(pattern, '', image_url)
     processed_url = re.sub('-scaled', '', processed_url)
-    
-    response = requests.get(processed_url)
+    return processed_url
+
+# 下载图片的函数
+def download_image(image_url, save_folder, txt_file):
+    response = requests.get(image_url)
     if response.status_code == 200:
-        file_name = os.path.join(save_folder, processed_url.split('/')[-1])
+        file_name = os.path.join(save_folder, image_url.split('/')[-1])
         with open(file_name, 'wb') as f:
             f.write(response.content)
         file_size = os.path.getsize(file_name)
-        current_time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
         with open(txt_file, 'a') as tf:
-            tf.write(f"{processed_url} | {file_size} bytes | {current_time}\n")
-        print(f"Downloaded {processed_url} to {file_name} | Size: {file_size} bytes")
+            tf.write(f"{image_url} | {file_size} bytes | {current_time}\n")
+        print(f"Downloaded {image_url} to {file_name} | Size: {file_size} bytes")
         return file_name, file_size, current_time
     else:
-        print(f"Failed to download {processed_url}")
+        print(f"Failed to download {image_url}")
         return None, None, None
 
 # 解析sitemap并下载图片
-def parse_sitemap(sitemap_url, save_folder, txt_file):
+def parse_sitemap(sitemap_url, save_folder_main, save_folder_thumb, txt_file_main, txt_file_thumb):
     response = requests.get(sitemap_url)
     soup = BeautifulSoup(response.text, 'xml')
     urls = soup.find_all('url')
     for url in urls:
+        # 下载<image:image>内的图片
+        image_tag = url.find('image:image')
+        if image_tag:
+            image_loc = image_tag.find('image:loc').text
+            processed_url = process_main_image_url(image_loc)
+            download_image(processed_url, save_folder_main, txt_file_main)
+        
+        # 下载<img>标签的srcset属性中最小尺寸的图片
         loc_tag = url.find('loc')
         if loc_tag:
             post_url = loc_tag.text
@@ -42,31 +53,28 @@ def parse_sitemap(sitemap_url, save_folder, txt_file):
             img_tag = post_soup.find('img', class_='attachment-post-thumbnail')
             if img_tag and 'srcset' in img_tag.attrs:
                 srcset = img_tag['srcset']
-                img_urls = srcset.split(', ')
-                # Find the smallest image
-                smallest_img_url = min(img_urls, key=lambda x: int(x.split(' ')[-2].replace('w', '')))[:-1]
-                download_image(smallest_img_url, save_folder, txt_file)
+                img_urls = re.findall(r'https?://\S+', srcset)
+                sizes = [int(re.search(r'(\d+)w', url).group(1)) for url in img_urls]
+                smallest_img_url = img_urls[sizes.index(min(sizes))]
+                download_image(smallest_img_url, save_folder_thumb, txt_file_thumb)
 
 # 主函数
-import datetime
-
 if __name__ == '__main__':
-    save_folder = 'downloaded_images'
-    txt_file = 'downloaded_images.txt'
-    readme_file = 'README.md'
-    if not os.path.exists(save_folder):
-        os.makedirs(save_folder)
-    if not os.path.exists(txt_file):
-        with open(txt_file, 'w') as tf:
+    save_folder_main = 'downloaded_images_main'
+    save_folder_thumb = 'downloaded_images_thumb'
+    txt_file_main = 'downloaded_images_main.txt'
+    txt_file_thumb = 'downloaded_images_thumb.txt'
+    
+    if not os.path.exists(save_folder_main):
+        os.makedirs(save_folder_main)
+    if not os.path.exists(save_folder_thumb):
+        os.makedirs(save_folder_thumb)
+    
+    if not os.path.exists(txt_file_main):
+        with open(txt_file_main, 'w') as tf:
             tf.write("Image URL | File Size | Download Time\n")
-    parse_sitemap(sitemap_url, save_folder, txt_file)
-
-    # Update README.md
-    with open(txt_file, 'r') as tf:
-        lines = tf.readlines()
-    with open(readme_file, 'r') as rf:
-        readme_content = rf.readlines()
-    # Insert the new downloads at the beginning of the README
-    new_content = lines[1:] + readme_content
-    with open(readme_file, 'w') as rf:
-        rf.writelines(new_content)
+    if not os.path.exists(txt_file_thumb):
+        with open(txt_file_thumb, 'w') as tf:
+            tf.write("Image URL | File Size | Download Time\n")
+    
+    parse_sitemap(sitemap_url, save_folder_main, save_folder_thumb, txt_file_main, txt_file_thumb)
