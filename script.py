49a7cf490567f1b38e401112ea9ac2aafa373f63
Map987 <99967856+Map987@users.noreply.github.com>
2024-04-20 08:35:13 +0800

    Update script.py

--- script.py
+++ script.py
@@ -2,9 +2,6 @@ import os
 import requests
 from bs4 import BeautifulSoup
 
-# sitemap URL
-sitemap_url = 'http://cineport.jp/post-sitemap.xml'
-
 # 下载图片的函数
 def download_image(image_url, save_folder):
     response = requests.get(image_url)
@@ -13,21 +10,17 @@ def download_image(image_url, save_folder):
         with open(file_name, 'wb') as f:
             f.write(response.content)
         print(f"Downloaded {image_url} to {file_name}")
-
-# 解析sitemap并下载图片
-def parse_sitemap(sitemap_url, save_folder):
-    response = requests.get(sitemap_url)
-    soup = BeautifulSoup(response.text, 'xml')
-    urls = soup.find_all('url')
-    for url in urls:
-        image_tag = url.find('image:image')
-        if image_tag:
-            image_loc = image_tag.find('image:loc').text
-            download_image(image_loc, save_folder)
+    else:
+        print(f"Failed to download {image_url}")
 
 # 主函数
 if __name__ == '__main__':
-    save_folder = 'downloaded_images'
+    save_folder = 'test_images'
     if not os.path.exists(save_folder):
         os.makedirs(save_folder)
-    parse_sitemap(sitemap_url, save_folder)
+    
+    # 指定的图片URL
+    image_url = "https://cdn.aqdstatic.com:966/age/20240045.jpg"
+    
+    # 下载图片
+    download_image(image_url, save_folder)
